7f3c291366f15f1862cb79e139519a81a9f2cd08
AVTAR SINGH <avtarsingh@AVTARs-MacBook-Air.local>
2025-02-18 05:59:21 +0530

    create-tree

---
+++ commands/write-tree.js
@@ -0,0 +1,14 @@
+const createTree = require("../app/create-tree");
+
+const getCreateTreeInputs = () => {
+   
+       createTree();
+        console.log("\nUsage:");
+        console.log("  bit create-tree");
+        console.log("\nExample:");
+        console.log("  bit create-tree");
+        return;
+
+}
+
+module.exports = getCreateTreeInputs;
--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 #!/usr/bin/env node
-const createBitDirectory = require('./app/init');
-const getCatFileInputs = require('./commands/cat-file');
-const getHashObjectInputs = require('./commands/hash-object');
-const getPrintTreeInputs = require('./commands/ls-tree');
+const createBitDirectory = require("./app/init");
+const getCatFileInputs = require("./commands/cat-file");
+const getHashObjectInputs = require("./commands/hash-object");
+const getPrintTreeInputs = require("./commands/ls-tree");
+const getCreateTreeInputs = require("./commands/write-tree");
 
 const command = process.argv[2];
 
 switch (command) {
-    case 'init':
-        createBitDirectory();
-        break;
+  case "init":
+    createBitDirectory();
+    break;
 
-    case 'add':
-        addFile();
-        break;
-    case 'read-file':
-        getCatFileInputs();
-        break;
-    case 'hash-my-file':
-        getHashObjectInputs();
-        break;
-    case 'print-tree':
-        getPrintTreeInputs();
-        break;
-    default:
-        console.log(`Unknown command: ${command}`);
-        console.log('Available commands:');
-        console.log('  init - Initialize a new Bit repository');
-        console.log('  add  - Add a file to the Bit repository');
-        break;
+  case "add":
+    addFile();
+    break;
+  case "read-file":
+    getCatFileInputs();
+    break;
+  case "hash-my-file":
+    getHashObjectInputs();
+    break;
+  case "print-tree":
+    getPrintTreeInputs();
+    break;
+  case "create-tree":
+    getCreateTreeInputs();
+    break;
+  default:
+    console.log(`Unknown command: ${command}`);
+    console.log("Available commands:");
+    console.log("  init - Initialize a new Bit repository");
+    console.log("  add  - Add a file to the Bit repository");
+    break;
 }
